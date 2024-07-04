bb57ac674882741fceaeddcd1b4a00d888a4eb76
rajendratechment <rajendra.verma@techment.com>
2024-07-04 11:41:43 +0530

    first commit

---
+++ lib/database/table/NoteFields.dart
@@ -0,0 +1,28 @@
+import 'package:persist_data/database/NoteDatabase.dart';
+
+import '../../model/NoteModel.dart';
+
+class NoteFields {
+  static const String tableName = 'notes';
+  static const String idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+  static const String textType = 'TEXT NOT NULL';
+  static const String intType = 'INTEGER NOT NULL';
+  static const String id = '_id';
+  static const String title = 'title';
+  static const String number = 'number';
+  static const String content = 'content';
+  static const String isFavorite = 'is_favorite';
+  static const String createdTime = 'created_time';
+
+
+  static const List<String> values = [
+    id,
+    number,
+    title,
+    content,
+    isFavorite,
+    createdTime,
+  ];
+
+
+}
